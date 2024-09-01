206a3b9545391c8712ee5867c28f46cb4a5a33f0
nielit-clubs <wnfc.nielit@gmail.com>
2024-09-01 13:56:26 +0530

    Create mailingSystem.js
    Adding mailing system

---
+++ js/mailingSystem.js
@@ -0,0 +1,11 @@
+function SendMail(){
+  var params = {
+    from_name: document.getElementById('fullname').value,
+    email_id : document.getElementById('email_id').value,
+    message: document.getElementById('message').value
+  }
+
+  emailjs.send("service_ejouusr", "template_6jsor7e", params).then(function(res){
+    alert("Success! " + res.status);
+  })
+}
